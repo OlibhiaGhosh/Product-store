e9ac566c90299a1ea36c241fd8c6e1414c8fb866
Olibhia Ghosh <olibhiag@gmail.com>
2025-01-12 00:33:26 +0530

    fixed the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -54,7 +54,7 @@ function Navbar() {
           ) : null}
         </div>
         <Link to="/signup">
-          <div className="p-3 bg-blue-700 text-white font-bold rounded-md transform transition-transform hover:scale-105">
+          <div className="px-3 py-2 bg-blue-700 text-white font-bold rounded-md transform transition-transform hover:scale-105">
             SignUp
           </div>
         </Link>
